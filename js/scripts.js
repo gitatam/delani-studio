2901fe6ec01e4a2c6c5aa39146f9d23c4ff1e3dd
Mathew Gitata <gitataIT@gmail.com>
2019-08-26 08:05:40 +0300

    Add function to toggle between the respective description of each icon in the what to do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-$(document).ready(function()){
-  $('.work').click(function()){
-    $(this).children('.work-text').slideToggle();
-  }
-}
+$(document).ready(function(){
+  $('.work').click(function(){
+    $(this).children('.work-text').slideToggle(500);
+    $(this).children('.work-image').slideToggle(800);
+  });
+});
