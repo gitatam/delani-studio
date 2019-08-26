9da03b77d2b3bc45244a19c0f9801a0aaf077246
Mathew Gitata <gitataIT@gmail.com>
2019-08-26 12:02:46 +0300

    Extend the hover effect to include all portfolio sections

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,46 @@ $(document).ready(function(){
     $('.card-title1').hide();
   });
 
+  $('.work-two').mouseover(function(){
+    $('.card-title2').show();
+  }).mouseout(function(){
+    $('.card-title2').hide();
+  });
+
+  $('.work-three').mouseover(function(){
+    $('.card-title3').show();
+  }).mouseout(function(){
+    $('.card-title3').hide();
+  });
+
+  $('.work-four').mouseover(function(){
+    $('.card-title4').show();
+  }).mouseout(function(){
+    $('.card-title4').hide();
+  });
+
+  $('.work-five').mouseover(function(){
+    $('.card-title5').show();
+  }).mouseout(function(){
+    $('.card-title5').hide();
+  });
+
+  $('.work-six').mouseover(function(){
+    $('.card-title6').show();
+  }).mouseout(function(){
+    $('.card-title6').hide();
+  });
+
+  $('.work-seven').mouseover(function(){
+    $('.card-title7').show();
+  }).mouseout(function(){
+    $('.card-title7').hide();
+  });
+
+  $('.work-eight').mouseover(function(){
+    $('.card-title8').show();
+  }).mouseout(function(){
+    $('.card-title8').hide();
+  });
+
 });
