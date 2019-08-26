99bab45ccac88e01f69e590fac1a5b94cf57852a
Mathew Gitata <gitataIT@gmail.com>
2019-08-26 11:54:14 +0300

    Add hover effect to portolio sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
+  //what we do toggle effect
   $('.work').click(function(){
-    $(this).children('.work-text').slideToggle(500);
-    $(this).children('.work-image').slideToggle(800);
+    $(this).children('.work-text').slideToggle("slow");
+    $(this).children('.work-image').slideToggle("slow");
   });
-  
+
+  //portfolio elements' hover effect
+  $('.work-one').mouseover(function(){
+    $('.card-title1').show();
+  }).mouseout(function(){
+    $('.card-title1').hide();
+  });
+
 });
